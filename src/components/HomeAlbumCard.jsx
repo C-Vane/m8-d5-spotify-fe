0fc4dd6a8583ee83c6b7a51c5f9297083cfdbe11
arzhk <72197750+arzhk@users.noreply.github.com>
2021-02-05 11:40:59 +0000

    Updated functionality
    - Updated home fetching to be faster and more reliable.

--- src/components/HomeAlbumCard.jsx
+++ src/components/HomeAlbumCard.jsx
@@ -5,10 +5,10 @@ import { Col } from "react-bootstrap";
 function HomeAlbumCard({ album }) {
   return (
     <Col sm={12} md={6} lg={4} xl={3} className="col-xxl-2 mb-2 pr-3 pr-md-2 px-lg-2 fade-in">
-      <Link to={"/album/" + album.id}>
+      <Link to={"/album/" + album.album.id}>
         <div className="album-card">
-          <img src={album.cover_medium} className="img-fluid" alt="album-art" />
-          <h5>{album.title}</h5>
+          <img src={album.album.cover_medium} className="img-fluid" alt="album-art" />
+          <h5>{album.album.title}</h5>
           <p>{album.artist.name}</p>
         </div>
       </Link>
