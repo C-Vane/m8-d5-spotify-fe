ad41ddcf6da81fbc9b1cc48ccab8a3a65e25d19c
arzhk <72197750+arzhk@users.noreply.github.com>
2021-02-05 11:47:21 +0000

    Updated functionality
    - Added content fetch to podcast tab on homepage

--- src/components/hometabs/PodcastTabContent.jsx
+++ src/components/hometabs/PodcastTabContent.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Spinner } from "react-bootstrap";
+import HomePlaylistAlbumCard from "../HomePlaylistAlbumCard";
 
 function PodcastTabContent() {
+  const [topPodcasts, setTopPodcasts] = useState([]);
+
+  const [topPodcastsLoaded, setTopPodcastsLoaded] = useState(false);
+
+  const fetchAlbumDataHandler = async (endpoint) => {
+    try {
+      const response = await fetch(
+        `https://yabba-dabba-duls-cors-anywhere.herokuapp.com/https://api.deezer.com/${endpoint}`
+      );
+
+      const data = await response.json();
+      if (!data.error) {
+        return data.podcasts.data;
+      } else {
+        alert("There was an error when fetching");
+      }
+    } catch (e) {
+      console.error(`API ERROR : ${e.message}`);
+    }
+  };
+
+  const start = async () => {
+    setTopPodcasts((await fetchAlbumDataHandler("chart")).splice(0, 15));
+    setTopPodcastsLoaded(true);
+  };
+
+  React.useEffect(() => {
+    start();
+  }, []);
+
   return (
     <div className="tab-pane fade show active" id="podcast" role="tabpanel" aria-labelledby="podcast-tab">
       <h2 className="mb-4 pb-2">#Podcast</h2>
@@ -8,36 +40,20 @@ function PodcastTabContent() {
         <div className="album-header-wrapper d-flex justify-content-between align-items-center">
           <h3>Top podcasts</h3>
         </div>
-        {/* TOP PODCASTS */}
         <div id="top-podcasts-container" className="mb-0 mb-xl-4">
           <div id="top-podcasts-row" className="row mb-0 mb-xl-4">
-            {/* Generate cards here */}
-          </div>
-        </div>
-        {/* TOP PODCASTS COLLAPSE CONTAINER */}
-        <div className="collapse" id="topPodcasts">
-          <div className="expand-section">
-            <div id="top-podcasts-expand-section" className="row mb-0 mb-xl-4">
-              {/* Generate cards here */}
-            </div>
-          </div>
-        </div>
-        {/* DYNAMIC PODCAST DUOS CONTENT */}
-        <div className="album-header-wrapper d-flex justify-content-between align-items-center">
-          <h3>Dynamic podcast duos</h3>
-        </div>
-        {/* DYNAMIC PODCAST DUOS ALBUMS */}
-        <div id="dynamic-duos-albums-container" className="mb-0 mb-xl-4">
-          <div id="dynamic-duos-row" className="row mb-0 mb-0 mb-xl-4">
-            {/* Generate cards here */}
-          </div>
-        </div>
-        {/* DYNAMIC PODCAST DUOS COLLAPSE CONTAINER */}
-        <div className="collapse" id="dynamicDuos">
-          <div className="expand-section">
-            <div id="dynamic-duos-expand-section" className="row mb-0 mb-xl-4">
-              {/* Generate cards here */}
-            </div>
+            {topPodcastsLoaded ? (
+              topPodcasts
+                .filter((e) => e !== undefined)
+                .map((playlist, index) => <HomePlaylistAlbumCard key={index} playlist={playlist} />)
+            ) : (
+              <>
+                <h5 className="d-inline-block mb-0 mr-2 ml-3" style={{ color: "white" }}>
+                  Loading...
+                </h5>
+                <Spinner animation="border" variant="primary" disabled />
+              </>
+            )}
           </div>
         </div>
       </div>
