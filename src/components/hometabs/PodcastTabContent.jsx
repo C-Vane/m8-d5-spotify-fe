9354cd89b663c60dd7fa3a7e3b199fb23457a1b3
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2021-02-19 17:02:26 +0100

    finished

--- src/components/hometabs/PodcastTabContent.jsx
+++ src/components/hometabs/PodcastTabContent.jsx
@@ -14,16 +14,16 @@ function PodcastTabContent(props) {
   const [topPodcasts, setTopPodcasts] = useState([]);
 
   const [topPodcastsLoaded, setTopPodcastsLoaded] = useState(false);
-
-  const fetchAlbumDataHandler = async (endpoint) => {
+  const fetchAlbumDataHandler = async (endp) => {
     try {
-      const response = await fetch(
-        `https://yabba-dabba-duls-cors-anywhere.herokuapp.com/https://api.deezer.com/${endpoint}`
-      );
-
-      const data = await response.json();
-      if (!data.error) {
-        return data.podcasts.data;
+      const response = await fetch(process.env.REACT_APP_BE_URL + "/music" + endp);
+      console.log(response);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        if (data) {
+          return data.splice(0, 15);
+        }
       } else {
         alert("There was an error when fetching");
       }
@@ -33,7 +33,7 @@ function PodcastTabContent(props) {
   };
 
   const startNew = async () => {
-    await setTopPodcasts((await fetchAlbumDataHandler("chart")).splice(0, 15));
+    await setTopPodcasts(await fetchAlbumDataHandler("/podcast"));
     setTopPodcastsLoaded(true);
 
     props.storeFetch(topPodcasts);
@@ -61,24 +61,22 @@ function PodcastTabContent(props) {
   }, [podcast]);
 
   return (
-    <div className="tab-pane fade show active" id="podcast" role="tabpanel" aria-labelledby="podcast-tab">
-      <h2 className="mb-4 pb-2">#Podcast</h2>
-      <div className="content-wrapper swing-in-top-fwd">
-        <div className="album-header-wrapper d-flex justify-content-between align-items-center">
+    <div className='tab-pane fade show active' id='podcast' role='tabpanel' aria-labelledby='podcast-tab'>
+      <h2 className='mb-4 pb-2'>#Podcast</h2>
+      <div className='content-wrapper swing-in-top-fwd'>
+        <div className='album-header-wrapper d-flex justify-content-between align-items-center'>
           <h3>Top podcasts</h3>
         </div>
-        <div id="top-podcasts-container" className="mb-0 mb-xl-4">
-          <div id="top-podcasts-row" className="row mb-0 mb-xl-4">
+        <div id='top-podcasts-container' className='mb-0 mb-xl-4'>
+          <div id='top-podcasts-row' className='row mb-0 mb-xl-4'>
             {topPodcastsLoaded ? (
-              topPodcasts
-                .filter((e) => e !== undefined)
-                .map((playlist, index) => <HomePlaylistAlbumCard key={index} playlist={playlist} />)
+              topPodcasts.filter((e) => e !== undefined).map((playlist, index) => <HomePlaylistAlbumCard key={index} playlist={playlist} />)
             ) : (
               <>
-                <h5 className="d-inline-block mb-0 mr-2 ml-3" style={{ color: "white" }}>
+                <h5 className='d-inline-block mb-0 mr-2 ml-3' style={{ color: "white" }}>
                   Loading...
                 </h5>
-                <Spinner animation="border" variant="primary" disabled />
+                <Spinner animation='border' variant='primary' disabled />
               </>
             )}
           </div>
